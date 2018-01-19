998f4a42365a3a2ea511f9be1ac496193284d390
Alex Vierzba <alexvierzba@hotmail.com>
2018-01-19 17:22:22 -0600

    Fixed glass pane block connection

--- src/main/java/funnelVortex/_3Decor/objects/blocks/DirectionalBlockBase.java
+++ src/main/java/funnelVortex/_3Decor/objects/blocks/DirectionalBlockBase.java
@@ -8,13 +8,18 @@ import funnelVortex._3Decor.util.IHasModel;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockDirectional;
 import net.minecraft.block.BlockHorizontal;
+import net.minecraft.block.BlockLeaves;
+import net.minecraft.block.BlockShulkerBox;
+import net.minecraft.block.BlockTrapDoor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
@@ -23,6 +28,7 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.Mirror;
 import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.event.world.BlockEvent.PlaceEvent;
@@ -84,7 +90,7 @@ public class DirectionalBlockBase extends Block implements IHasModel
     {
         return getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
     }
-	
+    
     @Override
     public IBlockState withRotation(IBlockState state, Rotation rot)
     {
--- src/main/java/funnelVortex/_3Decor/objects/blocks/WoodBoards.java
+++ src/main/java/funnelVortex/_3Decor/objects/blocks/WoodBoards.java
@@ -3,6 +3,7 @@ package funnelVortex._3Decor.objects.blocks;
 import net.minecraft.block.BlockStairs;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
@@ -10,6 +11,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class WoodBoards extends DirectionalBlockBase{
@@ -19,4 +21,10 @@ public class WoodBoards extends DirectionalBlockBase{
 		// TODO Auto-generated constructor stub
 		this.setHardness(1);
 	}
+	
+	@Override
+    public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face)
+    {
+        return BlockFaceShape.UNDEFINED;
+    }
 }
