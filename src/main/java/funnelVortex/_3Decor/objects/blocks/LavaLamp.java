a9f265c7a22481b70647d895d735744f77e0df34
Alex Vierzba <alexvierzba@hotmail.com>
2018-01-24 17:06:53 -0600

    Corrected lava lamp

--- src/main/java/funnelVortex/_3Decor/objects/blocks/LavaLamp.java
+++ src/main/java/funnelVortex/_3Decor/objects/blocks/LavaLamp.java
@@ -2,15 +2,22 @@ package funnelVortex._3Decor.objects.blocks;
 
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.BlockRenderLayer;
 
 public class LavaLamp extends BlockBase {
 
 	public LavaLamp() {
 		super("block_lavalamp", Material.GLASS);
 		// TODO Auto-generated constructor stub
-		this.setLightLevel(16F);
-		this.setLightOpacity(15);
+		this.setLightLevel(4F);
+		//this.setLightOpacity(10);
 		this.setHardness(0);
 		this.translucent = true;
 	}
+	
+	@Override
+    public BlockRenderLayer getBlockLayer()
+    {
+        return BlockRenderLayer.TRANSLUCENT;
+    }
 }
